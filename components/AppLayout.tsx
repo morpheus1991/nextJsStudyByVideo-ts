7c7f99296de0cd0b619cd71087570d61285a80f3
조민혁 <chohyuk0725@gmail.com>
2021-12-23 14:16:12 +0900

    heroicons 사용

--- components/AppLayout.tsx
+++ components/AppLayout.tsx
@@ -11,8 +11,8 @@ const AppLayout = ({ children }: Props) => {
     setGnbOpen((prev) => !prev);
   };
   return (
-    <div>
-      <div className="flex justify-center">
+    <div className="h-screen ">
+      <div className="flex justify-center w-full">
         <div
           className={`w-screen h-screen bg-black md:hidden opacity-0 fixed transition-opacity duration-500 ${
             gnbOpen ? "opacity-70" : ""
@@ -27,6 +27,20 @@ const AppLayout = ({ children }: Props) => {
             onClick={gnbToggle}
           >
             <span className=" absolute left-full">button</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
           </button>
           <div
             className={`${
@@ -41,6 +55,20 @@ const AppLayout = ({ children }: Props) => {
               onClick={gnbToggle}
             >
               <span className=" absolute left-full">button</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
             </button>
             <ul className="md:space-x-10 md:flex ">
               <li className="border-t-2 border-transparent md:hover:border-slate-800">
@@ -75,7 +103,9 @@ const AppLayout = ({ children }: Props) => {
           </div>
         </div>
       </div>
-      <div>{children}</div>
+      <div className="w-full bg-orange-600 grow mt-4 flex flex-col">
+        <div className="w-lg bg-orange-900 flex flex-col">{children}</div>
+      </div>
     </div>
   );
 };
--- pages/faq/index.tsx
+++ pages/faq/index.tsx
@@ -29,7 +29,7 @@ function classNames(...classes: string[]) {
 export default function Faq() {
   return (
     <AppLayout>
-      <div className="bg-gray-50">
+      <div className="bg-gray-50 grow">
         <div className="max-w-7xl mx-auto py-12 px-4 sm:py-16 sm:px-6 lg:px-8">
           <div className="max-w-3xl mx-auto divide-y-2 divide-gray-200">
             <h2 className="text-center text-3xl font-extrabold text-cyan-900 sm:text-4xl">
